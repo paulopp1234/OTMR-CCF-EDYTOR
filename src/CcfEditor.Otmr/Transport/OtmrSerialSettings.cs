1707d7b292cd3b7b89345113aa1bc61fd399bebe
paulopp1234 <158767192+paulopp1234@users.noreply.github.com>
2026-08-20 22:22:29 +0100

    Add proven 38400 8N1 OTMR serial settings

---
+++ src/CcfEditor.Otmr/Transport/OtmrSerialSettings.cs
@@ -0,0 +1,24 @@
+using System.IO.Ports;
+
+namespace CcfEditor.Otmr.Transport;
+
+public sealed record OtmrSerialSettings(
+    string PortName,
+    int BaudRate,
+    int DataBits,
+    Parity Parity,
+    StopBits StopBits)
+{
+    public static OtmrSerialSettings Class171Bench(string portName) =>
+        new(portName, 38400, 8, Parity.None, StopBits.One);
+
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(PortName))
+            throw new ArgumentException("A serial port must be selected.", nameof(PortName));
+        if (BaudRate <= 0)
+            throw new ArgumentOutOfRangeException(nameof(BaudRate));
+        if (DataBits is < 5 or > 8)
+            throw new ArgumentOutOfRangeException(nameof(DataBits));
+    }
+}
