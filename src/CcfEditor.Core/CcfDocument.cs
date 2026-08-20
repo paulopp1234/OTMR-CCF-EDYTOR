fcb8ffefa5497ae8e1878edfc2a556f273087cf4
paulopp1234 <158767192+paulopp1234@users.noreply.github.com>
2026-08-20 21:45:57 +0100

    Add byte change tracking for CCF editing

--- src/CcfEditor.Core/CcfDocument.cs
+++ src/CcfEditor.Core/CcfDocument.cs
@@ -28,9 +28,26 @@ public sealed class CcfDocument
     public string OriginalSha256 => CcfSha256.ComputeHex(_originalBytes);
     public string WorkingSha256 => CcfSha256.ComputeHex(_workingBytes);
     public bool IsByteIdenticalToOriginal => _originalBytes.AsSpan().SequenceEqual(_workingBytes);
+    public bool IsModified => !IsByteIdenticalToOriginal;
 
     public byte[] GetOriginalBytesSnapshot() => _originalBytes.ToArray();
     public byte[] GetWorkingBytesSnapshot() => _workingBytes.ToArray();
 
+    public IReadOnlyList<CcfByteChange> GetByteChanges()
+    {
+        var changes = new List<CcfByteChange>();
+        for (int offset = 0; offset < _workingBytes.Length; offset++)
+        {
+            if (_originalBytes[offset] != _workingBytes[offset])
+                changes.Add(new CcfByteChange(offset, _originalBytes[offset], _workingBytes[offset]));
+        }
+
+        return changes;
+    }
+
+    internal byte[] WorkingBytesBuffer => _workingBytes;
+    internal ReadOnlySpan<byte> OriginalBytesSpan => _originalBytes;
     internal ReadOnlySpan<byte> WorkingBytesSpan => _workingBytes;
 }
+
+public readonly record struct CcfByteChange(int Offset, byte OriginalValue, byte WorkingValue);
